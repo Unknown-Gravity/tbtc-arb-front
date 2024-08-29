24ad2aa26272fd64f151d4beb7d9c55d946cf6f8
Edunknown <eruiz@unknowngravity.com>
2024-08-29 15:26:29 +0200

    [FIX] fixed footer width

--- src/components/FooterComponent.tsx
+++ src/components/FooterComponent.tsx
@@ -6,6 +6,8 @@ const FooterComponent = () => {
 	return (
 		<Flex
 			w='100%'
+			maxW='1134px'
+			mx='auto'
 			justifyContent='space-between'
 			pb={{ xl: '218px' }}
 			gap={{ base: '20px' }}
